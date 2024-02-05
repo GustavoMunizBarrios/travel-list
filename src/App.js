e45fb2f0157d11bad234e44dda0250a4b441c04e
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2024-02-05 17:52:01 -0600

    Maping items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -27,7 +28,27 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item i={item} />
+          //<li>{item.description}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ i }) {
+  return (
+    <li>
+      <span style={i.packed ? { textDecoration: "line-through" } : {}}>
+        {i.quantity} {i.description}
+      </span>
+      <button onClick={() => alert("Eliminate Item?")}>✖</button>
+    </li>
+  );
 }
 
 function Stats() {
