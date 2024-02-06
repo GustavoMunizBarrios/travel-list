da3851b6a7418a6efde1b1bea8ca716d8b2ef8fe
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2024-02-05 21:25:32 -0600

    Added the Form element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 1, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -16,14 +16,28 @@ export default function App() {
 }
 
 function Logo() {
-  return <h1>🧳✈ Packing List</h1>;
+  return <h1>✈ Packing List ✈</h1>;
 }
 
 function Form() {
+  function handleSubmit(event) {
+    event.preventDefault(); // for avoiding reload the page
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select>
+        {/* Create an array from 1 to 20, and mapping 20 times "option" */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -32,7 +46,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item i={item} />
+          <Item i={item} key={item.id} />
           //<li>{item.description}</li>
         ))}
       </ul>
