47ff0bb0f56f063731e9846c79b542cd5e103c02
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2024-02-09 09:54:57 -0600

    I completed stats section

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItems={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -126,10 +126,28 @@ function Item({ i, onDeleteItem, onToggleItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      {percentage === 100 ? (
+        <em>You got everything! Ready to go ✈</em>
+      ) : (
+        <em>
+          You have {numItems} items on your list, and you already packed{" "}
+          {numPacked} ({percentage}%)
+        </em>
+      )}
     </footer>
   );
 }
